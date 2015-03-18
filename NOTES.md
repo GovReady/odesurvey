bee29930bb10bfc19079a2defbd166a43221121a
Greg Elin <greg@fotonotes.net>
2015-03-18 17:56:44 -0400

    Create Parse record when starting. Closes #13. Closes #12.

---
+++ NOTES.md
@@ -0,0 +1,42 @@
+NOTES
+=====
+
+Goal: Figure out a simple method to update the parse record using checklist code as example.
+Benefit: Have a partially working survey
+
+docs
+- slim
+- x-editable
+- checklist code (github)
+- bootstrap
+
+Plan: 
+√remove orgname common
+√create parse database
+√create config file
+√give others access to parse? Audrey and Gustavo
+√study q and checklist code for routine to push records
+√study x-editable
+xopen up x-editable console on page
+create unit test for writing record
+√try to create record on load
+√- open up q 
+try to update record
+
+Predict:
+3 hours
+learn: 
+- Opportunity to refactor checklist code
+- I should put in a test 
+- I don't have much error handling
+- do I use javascript or php backend to write info?
+
+risks
+- javascript vs. php > whatever works
+- won't work > did it before, give up after 30 mins and use time for something else
+- will want to keep going > stop when goal reached
+
+simpler alternative: hire someone
+- cant'f find them, need this for other items
+- do I have storing data working in 
+- I have storing working in other globalnet! in q!
--- html/index.php
+++ html/index.php
@@ -91,6 +91,93 @@ HTML;
 
 });
 
+// ************
+$app->get('/survey/opendata/:surveyId', function ($surveyId) use ($app) {
+	
+	$parse = new parseRestClient(array(
+	    'appid' => PARSE_APPLICATION_ID,
+	    'restkey' => PARSE_API_KEY
+	));
+
+	// # Let's try
+	// $params = array(
+	//     'className' => 'Audit',
+	//     'query' => array(
+ //        	'systemId' => $system['objectId']
+        	
+ //    	),
+ //    	'order' => 'securityCategory'
+	// );
+	// $request = $parse->query($params);
+	// $response = json_decode($request, true);
+	// $audits = $response['results'];
+
+	$content['surveyId'] = $surveyId;
+	$content['surveyName'] = "opendata";
+	$content['title'] = "Open Data Enterprise Survey";
+	$content['intro'] = <<<HTML
+
+        <blockquote>Second Survey Study
+        </blockquote>
+HTML;
+
+	$app->view()->setData(array('content' => $content ));
+	$app->render('survey/tp_home.php');
+
+});
+
+
+// ************
+$app->get('/survey/opendata/start/', function () use ($app) {
+	
+	$parse = new parseRestClient(array(
+	    'appid' => PARSE_APPLICATION_ID,
+	    'restkey' => PARSE_API_KEY
+	));
+	
+	$params = array("orgName", "orgType", "orgURL");
+	$object = array();
+    foreach ($params as $param) {
+    	if (!isset($allPostVars[$param])) {
+    		$allPostVars[$param] = null;
+    	}
+    	// $object[$param] = $allPostVars[$param];
+    	// if (in_array($param, array("daysRetainedStandard", "daysRetainedHeightened"))) {
+    	// 	$object[$param] = intval($object[$param]);
+    	// }
+    }
+
+    print_r($object);
+    
+	# store new information as new record 
+    $parse_params = array(
+        'className' => 'Org',
+        'object' => $object
+    );
+// Create Parse object and save
+    $request = $parse->create($parse_params);
+    $response = json_decode($request, true);
+
+
+    if(isset($response['objectId'])) {
+    	// Success
+    	$app->redirect("/survey/opendata/".$response['objectId']);
+    } else {
+    	// Failure
+    	echo "Problem. Promlem with record creation not yet handled.";
+    	exit;
+    	$app->redirect("/error".$response['objectId']);
+    }
+
+});
+
+// ************
+$app->get('/survey/opendata/', function () use ($app) {
+	
+    $app->redirect("/survey/opendata/start/");
+   
+
+});
 
 $app->run();
 
