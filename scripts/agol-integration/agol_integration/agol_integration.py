e9f837cad4c2836f57f130dc936e83140b6f857e
Brendan Collins <bcollins@blueraster.com>
2015-04-22 19:08:17 -0400

    added initial business logic and tests

---
+++ scripts/agol-integration/agol_integration/agol_integration.py
@@ -0,0 +1,39 @@
+# - std lib
+import os
+import sys
+
+# - 3rd party
+import pandas
+import requests
+
+# - local mods
+from settings import env
+
+import agol
+
+def get_parse_content(url):
+    content_response = requests.get(url)
+    content_response.raise_for_status()
+    if 'results' in content_response.json().keys():
+        df = pandas.DataFrame(content_response.json()['results'])
+        return df
+
+def refresh_agol(source_df, destination_feature_service_layer, token):
+    features = agol.dataframe_to_point_features(source_df, x_field='longitude', y_field='latitude', wkid=4326)
+    agol.delete_features(destination_feature_service_layer, where='1=1', token=token)
+    agol.add_features(destination_feature_service_layer, features, token=token)
+    return True
+
+def main(environment):
+    agol_token = agol.generate_token(env.agol_user, environment.agol_pass)
+    df = get_parse_content(environment.parse_data_endpoint)
+    refresh_agol(df, environment.agol_feature_service_url, token=agol_token)
+    return True
+
+if __name__ == '__main__':
+
+    main(env)
+
+
+
+
--- scripts/agol-integration/agol_integration/settings.py
+++ scripts/agol-integration/agol_integration/settings.py
@@ -1,30 +1,54 @@
 import os
 
-
 class BaseSettings(object):
 
     def __init__(self):
-        self.agol_user = '' # - add ArcGIS Online User ID
-        self.agol_pass = '' # - add ArcGIS Online User Pass
+        self.agol_user = os.environ['AGOL_USER'] # - add ArcGIS Online User ID or set environment variable
+        self.agol_pass = os.environ['AGOL_PASS'] # - add ArcGIS Online User Pass or set environment variable
+        self.parse_data_endpoint = 'http://ode.govready.org/survey/opendata/data/flatfile.json'        
 
 class DevelopmentSettings(BaseSettings):
 
     def __init__(self):
         BaseSettings.__init__(self)
-        self.agol_feature_service_url = ''
+        self.agol_feature_service_url = 'https://services5.arcgis.com/w1WEecz5ClslKH2Q/arcgis/rest/services/odesurvey_organizations_dev/FeatureServer/0'
 
 class StagingSettings(BaseSettings):
 
     def __init__(self):
         BaseSettings.__init__(self)
-        self.agol_feature_service_url = ''
+        self.agol_feature_service_url = 'https://services5.arcgis.com/w1WEecz5ClslKH2Q/arcgis/rest/services/odesurvey_organizations_staging/FeatureServer/0'
 
 class ProductionSettings(BaseSettings):
 
     def __init__(self):
         BaseSettings.__init__(self)
-        self.agol_feature_service_url = ''
+        self.agol_feature_service_url = 'https://services5.arcgis.com/w1WEecz5ClslKH2Q/arcgis/rest/services/odesurvey_organizations_production/FeatureServer/0'
 
+# - set active environment
 env = DevelopmentSettings()
 
+# - logging helper
+import logging
+import logging.handlers
 
+def get_logger(file_name, logger_name='AppLogger', level=logging.INFO, return_handlers=False):
+
+    # - logging setup
+    log_formater = logging.Formatter("%(asctime)s    %(threadName)s    %(levelname)s    %(message)s")
+    logger = logging.getLogger(logger_name)
+
+    file_handler = logging.handlers.RotatingFileHandler(file_name, maxBytes=1024*1024*10, backupCount=1)
+    file_handler.setFormatter(log_formater)
+    file_handler.setLevel(level)
+    logger.addHandler(file_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(log_formater)
+    stream_handler.setLevel(level)
+    logger.addHandler(stream_handler)
+
+    if return_handlers:
+        return logger, [file_handler, stream_handler]
+
+    return logger
