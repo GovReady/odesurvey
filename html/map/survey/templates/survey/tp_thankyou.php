ebb17d9bfed04eabdf620056ef12ca306f7d9679
Greg Elin <greg@fotonotes.net>
2015-05-18 08:02:34 -0400

    Fix thank you page text. Closes #116.

--- html/map/survey/templates/survey/tp_thankyou.php
+++ html/map/survey/templates/survey/tp_thankyou.php
@@ -13,34 +13,35 @@
 <!-- Main Content Section -->
 <div class="container lg-font col-md-12" style="border:0px solid black;">
 
- <form class="form-horizontal"><!-- using form tag for moment to preserve left side alignment -->
+    <div class="col-md-12" role="orgInfo-titlebar"  id="role-orgInfo-titlebar" style="margin-top: 40px;">
+      <div class="section-title"><h3>SUBMISION RECEIVED</h3></div>
+    </div>
+ 
+    <div class="col-md-12" role="orgInfo"  id="role-orgInfo" style="font-size:14pt;height:400px; margin-top">
+    
+    <div class="row col-md-12">
+        <div>
+          Thank you for participating in the Open Data Impact Map. <b>All submissions will be reviewed before public display.</b>  
+        </div>
+        <br />
+        <b>Take me to the Map</b>: <a href ="http://<?php echo $content['HTTP_HOST']; ?>/map/viz">http://<?php echo $content['HTTP_HOST']; ?>/map/viz</a>
+        <br /><br />
+        <b>View my submission</b>:  <a href ="http://<?php echo $content['HTTP_HOST']; ?>/map/survey/<?php echo $content['surveyId'] ?>">http://<?php echo $content['HTTP_HOST']; ?>/map/survey/<?php echo $content['surveyId'] ?></a>
+        <br />Use this unique URL to make changes.
 
-    <div class="row col-md-12 controlsec row-fluid" role="Intro">
-      <div class="row col-md-12">
-          <h3>SUMBISSION RECEIVED: Thank you!</h3>
-      </div>
+        <br /><br />
+        Please help us spread the word!<br />
+        <a href="https://twitter.com/intent/tweet?button_hashtag=&text=Checkout%20the%20open%20data%20impact%20map!%20http://opendataenterprise.org/map" class="twitter-hashtag-button" data-lang="en" data-size="large">Tweet</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+        <br />
+        If you have any questions, email us at map@odenterprise.org.
+        <br /><br />
+        Best, 
+        <br /><br />
+        The Center for Open Data Enterprise
+    </div>
 
-      <div class="row col-md-12">
-          <div>
-            Thank you for participating in the Open Data Impact Map! We will review and publish your submission shortly. 
-            If you wish to view or make any changes to your entry, you may access it via the unique url:  <a href ="http://<?php echo $content['HTTP_HOST']; ?>/map/survey/<?php echo $content['surveyId'] ?>">http://<?php echo $content['HTTP_HOST']; ?>/map/survey/<?php echo $content['surveyId'] ?></a>
-          </div>
-          <br />
-          Please help us spread the word!
-          <br />
-          <a href="https://twitter.com/intent/tweet?button_hashtag=&text=Checkout%20the%20open%20data%20impact%20map!%20http://opendataenterprise.org/map" class="twitter-hashtag-button" data-lang="en" data-size="large">Tweet</a>
-          <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-          <br /><br />
-          If you have any questions, email us at map@odenterprise.org.
-          <br /><br />
-          Best, 
-          <br /><br />
-          The Center for Open Data Enterprise
-      </div>
-
-    </div><!--/Intro-->
-
-</form>
+  </div><!--/Intro-->
 
 </div>
 
