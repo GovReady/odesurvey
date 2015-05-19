62c91b4cfddf0338fde931b2dabef73830047ab7
Greg Elin <greg@fotonotes.net>
2015-05-19 05:09:54 -0400

    Add social media buttons thank you page

--- html/map/survey/templates/survey/tp_thankyou.php
+++ html/map/survey/templates/survey/tp_thankyou.php
@@ -17,7 +17,7 @@
       <div class="section-title"><h3>SUBMISION RECEIVED</h3></div>
     </div>
  
-    <div class="col-md-12" role="orgInfo"  id="role-orgInfo" style="font-size:14pt;height:400px; margin-top">
+    <div class="col-md-12" role="orgInfo"  id="role-orgInfo" style="font-size:14pt;height:460px; margin-top">
     
     <div class="row col-md-12">
         <div>
@@ -30,10 +30,28 @@
         <br />Use this unique URL to make changes.
 
         <br /><br />
-        Please help us spread the word!<br />
-        <a href="https://twitter.com/intent/tweet?button_hashtag=&text=Checkout%20the%20open%20data%20impact%20map!%20http://opendataenterprise.org/map" class="twitter-hashtag-button" data-lang="en" data-size="large">Tweet</a>
-        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-        <br />
+        Please help us spread the word!<br /><br />
+        
+          <div class="w-row share-row">
+            <div class="w-col w-col-3 w-col-small-3 w-col-tiny-3">
+              <a class="w-inline-block share-icon" href="https://twitter.com/home?status=Check%20out%20the%20%23OpenData%20Impact%20Map,%20a%20global%20view%20of%20open%20data%20use%20cases%20at%20http://opendataenterprise.org/map%20%23ODImpactMap%20%40odenterprise" target="_blank"><img src="/images/Share-twitter.png">
+              </a>
+            </div>
+            <div class="w-col w-col-3 w-col-small-3 w-col-tiny-3">
+              <a class="w-inline-block share-icon" href="https://www.facebook.com/sharer/sharer.php?u=http://opendataenterprise.org/map" target="_blank"><img src="/images/Share-facebook.png">
+              </a>
+            </div>
+            <div class="w-col w-col-3 w-col-small-3 w-col-tiny-3">
+              <a class="w-inline-block share-icon" href="https://www.linkedin.com/shareArticle?mini=true&amp;url=http://opendataenterprise.org/map&amp;title=&amp;summary=Check%20out%20the%20Open%20Data%20Impact%20Map,%20a%20global%20view%20of%20open%20data%20use%20cases%20at%0Ahttp://opendataenterprise.org/map%0A%23ODImpactMap%0A%40odenterprise&amp;source=" target="_blank"><img src="/images/Share-linkedin.png">
+              </a>
+            </div>
+            <div class="w-col w-col-3 w-col-small-3 w-col-tiny-3">
+              <a class="w-inline-block share-icon" href="mailto:?&amp;subject=Open Data Impact Map&amp;body=Check%20out%20the%20Open%20Data%20Impact%20Map,%20a%20global%20view%20of%20open%20data%20use%20cases%20at%20http://opendataenterprise.org/map%20%23ODImpactMap%20%40odenterprise" target="_blank"><img src="/images/Share-email.png">
+              </a>
+            </div>
+          </div>
+          <br /><br />
+
         If you have any questions, email us at map@odenterprise.org.
         <br /><br />
         Best, 
