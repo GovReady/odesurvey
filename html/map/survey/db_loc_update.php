49b3f382ac6c4d3b49876747eabbe353d72759a9
Myeong Lee <myeong@Myeongui-MBP.home>
2015-10-15 05:06:23 -0400

    enhanced version of cleaning the data

--- html/map/survey/db_loc_update.php
+++ html/map/survey/db_loc_update.php
@@ -37,56 +37,73 @@ require ('vendor/parse.com-php-library/parse.php');
 // Include application functions
 require 'functions.inc.php';
 
+// update_db("org_hq_country", "blank");
+// update_db("org_hq_country", "undefined");
+// update_db("org_hq_country_region_code", "blank");
+// update_db("org_hq_country_region_code", "undefined");
+// update_db("org_hq_country_region", "blank");
+// update_db("org_hq_country_region", "undefined");
+// update_db("org_hq_country_income_code", "blank");
+// update_db("org_hq_country_income_code", "undefined");
+// update_db("org_hq_country_income", "blank");
+// update_db("org_hq_country_income", "undefined");
+update_db("org_hq_country_locode", "defined", 0);
+update_db("org_hq_country_locode", "defined", 1);
 
-update_db("org_hq_country", "blank");
-update_db("org_hq_country", "undefined");
-update_db("org_hq_country_region_code", "blank");
-update_db("org_hq_country_region_code", "undefined");
-update_db("org_hq_country_region", "blank");
-update_db("org_hq_country_region", "undefined");
-update_db("org_hq_country_income_code", "blank");
-update_db("org_hq_country_income_code", "undefined");
-update_db("org_hq_country_income", "blank");
-update_db("org_hq_country_income", "undefined");
 
-
-function update_db($missing_column, $condition){
+function update_db($missing_column, $condition, $loop){
 
   $query = new ParseQuery("org_profile");
   $query->setLimit(1000);
+  $query->setSkip($loop * 1000);
 
-  //$query->whereEqualTo("org_hq_country_income_code", "null");
+ 
   if ($condition == "undefined"){
     echo $missing_column . " for undefined." . "<br>";
     $query->whereEqualTo($missing_column, "");
   } elseif ($condition == "blank") {
     echo $missing_column . " for undefined." . "<br>";
     $query->whereDoesNotExist($missing_column);
+  } elseif ($condition == "defined") {
+    echo $missing_column . " for defined." . "<br>";
+    $query->whereExists($missing_column);
   }
 
   $results = $query->find();
 
-
+  $need_update = FALSE;
   $element = array();
   $i=0;
+
   foreach ($results as $object){
     foreach($object as $obj){  	
       $wb_region = addWbRegions($obj->org_hq_country_locode);
       
-      $element[$obj->objectId] = array(
-        "org_hq_country" => $wb_region['org_hq_country_name'],
+      if (!isset($obj->org_hq_country_locode) or $obj->org_hq_country_locode=="") continue;
+
+      $element[$obj->objectId] = array(        
         "org_hq_country_locode" => $obj->org_hq_country_locode,
-        'org_hq_country_region' => $wb_region['org_hq_country_region'],
-        'org_hq_country_region_code' => $wb_region['org_hq_country_region_code'],
-        'org_hq_country_income' => $wb_region['org_hq_country_income'],
-        'org_hq_country_income_code' => $wb_region['org_hq_country_income_code'],
       );
 
-      echo $obj->objectId;    
-      echo '<br>';
+
+      if (!isset($obj->org_hq_country) || !isset($obj->org_hq_country_region) || !isset($obj->org_hq_country_region_code) ||
+        !isset($obj->org_hq_country_income) || !isset($obj->org_hq_country_income_code)) {
+        $element[$obj->objectId]["org_hq_country"] = $wb_region['org_hq_country_name'];          
+        $element[$obj->objectId]["org_hq_country_region"] = $wb_region['org_hq_country_region'];                   
+        $element[$obj->objectId]["org_hq_country_region_code"] = $wb_region['org_hq_country_region_code'];          
+        $element[$obj->objectId]["org_hq_country_income"] = $wb_region['org_hq_country_income'];               
+        $element[$obj->objectId]["org_hq_country_income_code"] = $wb_region['org_hq_country_income_code'];   
+        $need_update = TRUE;       
+      }
+
+      if ($need_update){
+        echo $obj->objectId;    
+        echo '<br>';
+      }
+      $i++;
+      if ($i == 1000) break;
     }
-    $i++;
-    if ($i == 1000) break;
+    
   }
   echo '<br>';
 
@@ -94,25 +111,27 @@ function update_db($missing_column, $condition){
     echo "No data to update <br>";    
   }
 
-  $count = 0;
+  if ($need_update){
+    $count = 0;
 
-  foreach ($element as $key=>$value){
+    foreach ($element as $key=>$value){
 
-    $parse = new ParseObject('org_profile');
-    $parse->__set('objectId', $key);
-    $parse->__set('org_hq_country', $value["org_hq_country"]);
-    $parse->__set('org_hq_country_locode', $value["org_hq_country_locode"]);
-    $parse->__set('org_hq_country_region', $value["org_hq_country_region"]);
-    $parse->__set('org_hq_country_region_code', $value["org_hq_country_region_code"]);
-    $parse->__set('org_hq_country_income', $value["org_hq_country_income"]);
-    $parse->__set('org_hq_country_income_code', $value["org_hq_country_income_code"]);
-    $request = $parse->update($key);
-    $count++;
+      $parse = new ParseObject('org_profile');
+      $parse->__set('objectId', $key);
+      $parse->__set('org_hq_country', $value["org_hq_country"]);
+      $parse->__set('org_hq_country_locode', $value["org_hq_country_locode"]);
+      $parse->__set('org_hq_country_region', $value["org_hq_country_region"]);
+      $parse->__set('org_hq_country_region_code', $value["org_hq_country_region_code"]);
+      $parse->__set('org_hq_country_income', $value["org_hq_country_income"]);
+      $parse->__set('org_hq_country_income_code', $value["org_hq_country_income_code"]);
+      $request = $parse->update($key);
+      $count++;
+    }
+
+
+    echo $missing_column . ": " . strval($count) . " completed<br>";
+    echo '<br>';
   }
-
-
-  echo $missing_column . ": " . strval($count) . " complete<br>";
-  echo '<br>';
 }
 
 
